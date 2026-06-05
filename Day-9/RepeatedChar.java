08b4bf82605ee5ca1ec305f558e1acbf7323c695
Keshav <keshavguptauidev@gmail.com>
2026-06-06 00:14:05 +0530

    Day-9 assignments

---
+++ Day-9/RepeatedChar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RepeatedChar {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(char i=0;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print((char)('A'+ i));
+            }
+            System.out.println();
+        }
+    }
+}
