21446fb8b88d1375bf60dc4a8513c4bda5801c29
Keshav <keshavguptauidev@gmail.com>
2026-06-09 18:52:29 +0530

    Day-13 assignments

---
+++ Day-13/DisplayArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DisplayArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i =0;i<N;i++){
+            System.out.print(arr[i] + ",");
+        }
+    }
+}
